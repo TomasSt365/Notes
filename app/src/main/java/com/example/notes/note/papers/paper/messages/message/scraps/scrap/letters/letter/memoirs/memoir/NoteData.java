0b53f6b0535cf2bcf3bbf42538344d800360ff11
Tomas.St <lapink540@gmail.com>
2021-08-09 12:44:41 +0300

    Создание класса для даных заметки

---
+++ app/src/main/java/com/example/notes/note/papers/paper/messages/message/scraps/scrap/letters/letter/memoirs/memoir/NoteData.java
@@ -0,0 +1,20 @@
+package com.example.notes.note.papers.paper.messages.message.scraps.scrap.letters.letter.memoirs.memoir;
+
+public class NoteData {
+    private String name;
+    private String noteText;
+    private String dateOfCreation;
+    private String dateOfChange;
+
+    public NoteData(String name, String noteText){
+        this.name = name;
+        this.noteText = noteText;
+    }
+
+    public NoteData(String name){
+        this.name = name;
+    }
+
+
+
+}
