0983179b5ceac5e79dd8a932fa6ff7ba543f50bd
Tomas.St <lapink540@gmail.com>
2021-08-22 18:24:04 +0300

    создание имплементации источника данных по Firestore
    P.S.: Случайно запушил рано новый файл. Эта версия будет не работать!!!)

---
+++ app/src/main/java/com/example/notes/note/papers/paper/messages/message/scraps/scrap/letters/letter/memoirs/memoir/data/NotesSourceFirebaseImpl.java
@@ -0,0 +1,97 @@
+package com.example.notes.note.papers.paper.messages.message.scraps.scrap.letters.letter.memoirs.memoir.data;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class NotesSourceFirebaseImpl implements NotesSource {
+
+    private static final String NOTES_COLLECTION = "NOTES_COLLECTION";
+
+    private final FirebaseFirestore store = FirebaseFirestore.getInstance();
+    private final CollectionReference collectionReference = store.collection(NOTES_COLLECTION);
+    private List<NoteData> notes = new ArrayList<NoteData>();
+
+    @Override
+    public int size() {
+        return notes.size();
+    }
+
+    @Override
+    public NoteData getNoteData(int position) {
+        return notes.get(position);
+    }
+
+    @Override
+    public void addNote(NoteData note) {
+        collectionReference
+                .add(NoteDataConvertor.noteDataToDocument(note));
+    }
+
+    @Override
+    public void editNote(int position, NoteData note) {
+        collectionReference
+                .document(notes.get(position).getId())
+                .update(NoteDataConvertor.noteDataToDocument(note));
+    }
+
+    @Override
+    public void deleteNote(int position) {
+        collectionReference
+                .document(notes.get(position).getId())
+                .delete();
+    }
+
+    @Override
+    public void clearAllNote() {
+        for (NoteData note : notes) {
+            collectionReference
+                    .document(note.getId())
+                    .delete();
+        }
+    }
+
+    @Override
+    public NotesSource getFavoriteData() {
+        List<NoteData> favoriteData = new ArrayList<>();
+        for (int i = 0; i < notes.size(); i++) {
+            if (this.getNoteData(i).isFavorite()) {
+                favoriteData.add(this.getNoteData(i));
+            }
+        }
+        return new NotesSourceImpl(favoriteData);
+    }
+
+    @Override
+    public NotesSource init(NotesSourceResponse notesSourceResponse) {
+        collectionReference.orderBy(NoteDataConvertor.Fields.DATE_OF_EDIT, Query.Direction.DESCENDING)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            notes = new ArrayList<NoteData>();
+                            for (QueryDocumentSnapshot docFields : Objects.requireNonNull(task.getResult())) {
+                                NoteData note = NoteDataConvertor
+                                        .documentToNoteData(docFields.getId(), docFields.getData());
+                                notes.add(note);
+                            }
+                            notesSourceResponse.initialized(NotesSourceFirebaseImpl.this);
+                        }
+                    }
+                });
+
+        return this;
+    }
+
+}
