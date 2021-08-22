36b061ee2bad7afe84daac1f312e6743f7611b32
Tomas.St <lapink540@gmail.com>
2021-08-22 11:40:37 +0300

    Добавление нового поля для класса данных, чтобы считывать их с сервера

--- app/src/main/java/com/example/notes/note/papers/paper/messages/message/scraps/scrap/letters/letter/memoirs/memoir/data/NoteData.java
+++ app/src/main/java/com/example/notes/note/papers/paper/messages/message/scraps/scrap/letters/letter/memoirs/memoir/data/NoteData.java
@@ -7,6 +7,7 @@ public class NoteData implements Parcelable {
     public static final byte TRUE = 1;
     public static final byte FALSE = 0;
 
+    private String id;
     private String title;
     private String noteContent;
     private String dateOfCreation;
@@ -14,6 +15,7 @@ public class NoteData implements Parcelable {
     private byte isFavorite;
 
     public NoteData(Builder builder) {
+        this.id = builder.id;
         this.title = builder.title;
         this.noteContent = builder.noteContent;
         this.dateOfCreation = builder.dateOfCreation;
@@ -21,62 +23,20 @@ public class NoteData implements Parcelable {
         this.isFavorite = builder.isFavorite;
     }
 
-    protected NoteData(Parcel in) {
-        title = in.readString();
-        noteContent = in.readString();
-        dateOfCreation = in.readString();
-        dateOfChange = in.readString();
-        isFavorite = in.readByte();
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(title);
-        dest.writeString(noteContent);
-        dest.writeString(dateOfCreation);
-        dest.writeString(dateOfChange);
-        dest.writeByte(isFavorite);
-    }
-
-    public static final Creator<NoteData> CREATOR = new Creator<NoteData>() {
-        @Override
-        public NoteData createFromParcel(Parcel in) {
-            return new NoteData(in);
-        }
-
-        @Override
-        public NoteData[] newArray(int size) {
-            return new NoteData[size];
-        }
-    };
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getNoteContent() {
-        return noteContent;
-    }
-
-    public byte isFavorite() {
-        return isFavorite;
-    }
-
-    public void setFavorite(byte favorite) {
-        isFavorite = favorite;
-    }
+    //==========================Builder================================//
 
     public static class Builder{
+        private String id;
         private String title = "";
         private String noteContent = "";
         private String dateOfCreation;
         private String dateOfChange;
         private byte isFavorite = FALSE;
+
+        public Builder setId(String id) {
+            this.id = id;
+            return this;
+        }
 
         public Builder setTitle(String title) {
             this.title = title;
@@ -107,4 +67,70 @@ public class NoteData implements Parcelable {
             return new NoteData(this);
         }
     }
+
+    //=================Getters and Setters=======================//
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getNoteContent() {
+        return noteContent;
+    }
+
+    public byte isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(byte favorite) {
+        isFavorite = favorite;
+    }
+
+    //=================Parcelable implementation=======================//
+
+    protected NoteData(Parcel in) {
+        id = in.readString();
+        title = in.readString();
+        noteContent = in.readString();
+        dateOfCreation = in.readString();
+        dateOfChange = in.readString();
+        isFavorite = in.readByte();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(title);
+        dest.writeString(noteContent);
+        dest.writeString(dateOfCreation);
+        dest.writeString(dateOfChange);
+        dest.writeByte(isFavorite);
+    }
+
+    public static final Creator<NoteData> CREATOR = new Creator<NoteData>() {
+        @Override
+        public NoteData createFromParcel(Parcel in) {
+            return new NoteData(in);
+        }
+
+        @Override
+        public NoteData[] newArray(int size) {
+            return new NoteData[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
 }
