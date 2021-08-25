9234910fb2d4fbb3c707a785055f117992c7a40b
Tomas.St <lapink540@gmail.com>
2021-08-25 12:42:20 +0300

    Создание интерфеса слушателя для диалогового окна

---
+++ app/src/main/java/com/example/notes/note/papers/paper/messages/message/scraps/scrap/letters/letter/memoirs/memoir/removeDialog/OnRemoveDialogClickListener.java
@@ -0,0 +1,6 @@
+package com.example.notes.note.papers.paper.messages.message.scraps.scrap.letters.letter.memoirs.memoir.removeDialog;
+
+public interface OnRemoveDialogClickListener {
+    void onPositiveButtonClick();
+    void onNegativeButtonClick();
+}
